b5499c7936f5f0a5c443a967cf48468ec6dc13a8
Javier Lizandra <javispi59@gmail.com>
2020-12-08 15:35:01 +0100

    Fin de la seccion 10 - Auth

--- database/config.js
+++ database/config.js
@@ -3,20 +3,19 @@ const mongoose = require('mongoose');
 
 const dbConnection = async() => {
 
-    try{
-        await mongoose.connect(process.env.DB_CNN, {
+    try {
+        await mongoose.connect( process.env.DB_CNN , {
             useNewUrlParser: true, 
             useUnifiedTopology: true,
             useCreateIndex: true
         });
 
-        console.log('BD Online')
-
+        console.log('DB Online');
+        
     } catch (error) {
         console.log(error);
         throw new Error('Error a la hora de iniciar la BD ver logs');
     }
-
 
 
 }
--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const express = require('express');
 require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
 
 const { dbConnection } = require('./database/config');
 
+// Crear el servidor de express
 const app = express();
 
+// Configurar CORS
+app.use( cors() );
+
+// Lectura y parseo del body
+app.use( express.json() );
 
 // Base de datos
 dbConnection();
 
 
-//User
-//mean_user
-//nBU7PbO2VfOecpyL
+// Rutas
+app.use( '/api/usuarios', require('./routes/usuarios') );
+app.use( '/api/login', require('./routes/auth') );
 
-//Rutas
-app.get( '/', (req, res) =>{
-
-    res.json({
-        ok: true,
-        msg: 'Hola Mundo'
-    });
-
-});
 
 
 app.listen( process.env.PORT, () => {
-    console.log('Servidor corriendo en puerto ' + process.env.PORT);
+    console.log('Servidor corriendo en puerto ' + process.env.PORT );
 });
+
